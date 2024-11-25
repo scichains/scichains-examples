de2e671475f02d84ce6bf8e785ae3b9d28f9c6b7
Daniel Alievsky <daniel.alievsky@gmail.com>
2024-11-25 15:22:48 +0200

    demo folder removed: moved to SciChains monitor build

--- demo/multi_language/js/js/modules/demo/ExampleStareInformation.mjs
+++
@@ -1,9 +0,0 @@
-export function execute(params, inputs, outputs) {
-    const e = params._sys.executor
-    const msg = "Hello! I am " + e.executorModel().getName() + ", id " + e.getExecutorId() + " - " + e
-    e.showStatus(msg)
-    print(msg)
-    e.defaultOutputPortName("model")
-    outputs.platform = params._sys.platform
-    outputs.model = e.executorModel().jsonString()
-}
--- demo/multi_language/js/js/modules/demo/scalars/ExampleScalarSum.mjs
+++
@@ -1,4 +0,0 @@
-export function execute(params, inputs, outputs) {
-    outputs.output = params.a + params.b
-    return "Hi" // ignored: overridden by outputs.output
-}
