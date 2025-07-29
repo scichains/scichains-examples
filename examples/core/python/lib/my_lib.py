41f24af5552ea61d9cfb5d7602978b6eeed1cb05
Daniel Alievsky <daniel.alievsky@gmail.com>
2025-07-29 13:21:49 +0300

    Testing

--- examples/core/python/lib/my_lib.py
+++ examples/core/python/lib/my_lib.py
@@ -1,4 +1,6 @@
 import numpy
+# - this is a usual Python module, we need to import "numpy" explicitly
+from pyalgart.api import import_file
 
 
 def my_function():
@@ -7,6 +9,8 @@ def my_function():
 def my_function_array():
     return numpy.array([1,2,3])
 
-def my_function_with_import(env):
-    m = env.import_file("lib/my_sub_lib.py")
+def my_function_with_import():
+    m = import_file("my_sub_lib.py")
+    # note: we may use a relative path instead of "lib/my_sub_lib.py"
+    # only if "lib" is specified as "Current working folder" in the executor
     return m.my_sub_function()
